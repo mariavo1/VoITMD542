332a91da4ad855ec92292a26ac67dffcd4b881f3
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-15 11:39:08 -0600

    Repo

--- lab2/express-lab2/src/contactsFileRepository.js
+++ lab2/express-lab2/src/contactsFileRepository.js
@@ -16,3 +16,27 @@ const saveData = () => {
     const stringifyData  = JSON.stringify(Array.from(db));
     fs.writeFileSync(path.join(__dirname , '../data/contact.json'), stringifyData);
 };
+
+const repo = {
+    findAll : () => Array.from(db.values()),
+    findById: (uuid) => db.get(uuid), 
+    create: (contact) => {
+        contact.id = crypto.randomUUID();
+        contact.date = new Date();
+        db.set(contact.id, contact);
+        saveData();
+    },
+    deleteById: (uuid) => {
+        db.delete(uuid)
+        saveData();
+    },
+    update: (contact) => {
+        contact.date = new Date();
+        db.set(contact.id, contact)
+        saveData();
+    },
+};
+
+loadData();
+
+module.exports = repo;
