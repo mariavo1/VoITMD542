f8b4a95ccfa9c78546360fd9da6b17d4b43c8339
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-15 11:00:38 -0600

    delete data array

--- lab2/express-lab2/routes/contact.js
+++ lab2/express-lab2/routes/contact.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-let data = [{
-    
-}];
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact Database' });
