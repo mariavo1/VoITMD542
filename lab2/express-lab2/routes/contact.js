2cf1b958810e4ed461b56cd61cb9dda011167e6c
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-15 11:18:31 -0600

    Update route contact.js

--- lab2/express-lab2/routes/contact.js
+++ lab2/express-lab2/routes/contact.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const contactsRepo = require('../src/contactRepo');
+const contactsFileRepo = require('../src/contactsFileRepository');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  const data = contactsFileRepo.findAll();
   res.render('contact', { title: 'Contact Database' });
 });
 
