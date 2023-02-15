d0529365d98f4f5137f5208d79488182c4ceec7a
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-15 11:31:44 -0600

    Create loadData function

--- lab2/express-lab2/src/contactsFileRepository.js
+++ lab2/express-lab2/src/contactsFileRepository.js
@@ -3,3 +3,11 @@ const fs = require('fs');
 const path = require('path');
 const db = new Map();
 const contactsRepo = require('./contactsRepo');
+
+const loadData = () => {
+    const jsonData = fs.readFileSync(path.join(__dirname , '../data/contact.json'));
+    const contactArray = JSON.parse(jsonData);
+    contactArray.forEach(element => {
+        db.set(element[0], element[1])
+    });
+};
