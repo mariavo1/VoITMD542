d5c89c166a84187387f2dd781469cb4c78b184d7
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-15 10:56:21 -0600

    Create data model array

--- lab2/express-lab2/routes/contact.js
+++ lab2/express-lab2/routes/contact.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+let data = [{
+    
+}];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact Database' });
